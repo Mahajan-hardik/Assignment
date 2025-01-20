69f6e69aa32952d5adbd67bcfe8034bd78801440
SHROUD <95184488+SHROUDIEE@users.noreply.github.com>
2025-01-20 14:42:37 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,15 @@
+public class armstrong{
+    public static void main(String[] args){
+        int count=0;
+        int n=152;
+        int sum=0,digit=0;
+        while(n>0){
+            digit = N%10;
+            sum+=digit;
+            int length = String.valueOf(n).length();
+        }
+
+         }
+        
+        System.out.println();
+}
