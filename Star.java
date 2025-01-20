6fe914e57bdf0d5728a2592e5618dd7b48105f6e
SHROUD <95184488+SHROUDIEE@users.noreply.github.com>
2025-01-20 15:04:37 +0530

    Create Star.java

---
+++ Star.java
@@ -0,0 +1,14 @@
+public class star{
+    public static void main(String[] args){
+        int str=1;
+        int N=8;
+        for(int i=0;i<=N;i++){
+
+            for(int j=0;j<=i;j++)
+            {
+                 System.out.print("*");
+            }
+                System.out.print("\n");
+        }
+    }
+}
