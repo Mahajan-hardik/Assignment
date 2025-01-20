b1b7a45ecdad158175bbf053d73d23770441b8c8
SHROUD <95184488+SHROUDIEE@users.noreply.github.com>
2025-01-20 14:55:35 +0530

    Create oddNumbersFrom1_100.java

---
+++ oddNumbersFrom1_100.java
@@ -0,0 +1,10 @@
+class odd{
+    public static void main(String[] args){
+        //int odd=0;
+        for(int i =0;i<100;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
